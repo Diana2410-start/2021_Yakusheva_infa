f2d3ce77fd352e3271b549eaff9391dc2d26e304
Diana2410-start <yakusheva2410@yandex.ru>
2021-03-22 11:44:45 +0300

    1 часть

--- lab pushka/1.py
+++ lab pushka/1.py
@@ -11,10 +11,6 @@ root.geometry('800x600')
 canv = tk.Canvas(root, bg='white')
 canv.pack(fill=tk.BOTH, expand=1)
 
-screen1 = canv.create_text(400, 300, text='', font='28')
-bullet = 0
-balls = []
-
 
 class ball():
     def __init__(self, x=40, y=450):
@@ -57,10 +53,10 @@ class ball():
         """
         # FIXME
         self.x += self.vx
-        self.y += self.vy
+        self.y -= self.vy
         self.set_coords()
 
-    def hittest(self, obj):
+    def hittest(self, object):
         """Функция проверяет сталкивалкивается ли данный обьект с целью, описываемой в обьекте obj.
 
         Args:
@@ -68,7 +64,15 @@ class ball():
         Returns:
             Возвращает True в случае столкновения мяча и цели. В противном случае возвращает False.
         """
+
         # FIXME
+        if ((self.x - object.x) ** 2 + (self.y - object.y) ** 2) ** 0.5 <= self.r + object.r:
+            return True
+        return False
+
+
+
+
 
 
 class gun():
@@ -76,9 +80,7 @@ class gun():
         self.f2_power = 10
         self.f2_on = 0
         self.an = 1
-        self.x = 20
-        self.y = 450
-        self.id = canv.create_line(self.x, self.y, 50, 420, width=7)  # FIXME: don't know how to set it...
+        self.id = canv.create_line(20, 450, 50, 420, width=7)  # FIXME: don't know how to set it...
 
     def fire2_start(self, event):
         self.f2_on = 1
@@ -94,17 +96,17 @@ class gun():
         new_ball = ball()
         new_ball.r += 5
         self.an = math.atan((event.y - new_ball.y) / (event.x - new_ball.x))
-
         new_ball.vx = self.f2_power * math.cos(self.an)
         new_ball.vy = - self.f2_power * math.sin(self.an)
-
-
         balls += [new_ball]
         self.f2_on = 0
         self.f2_power = 10
 
     def targetting(self, event=0):
         """Прицеливание. Зависит от положения мыши."""
+        x2 = 20 + max(self.f2_power, 20) * math.cos(self.an)
+        y2 = 450 + max(self.f2_power, 20) * math.sin(self.an)
+
         if event:
             self.an = math.atan((event.y - 450) / (event.x - 20))
         if self.f2_on:
@@ -112,8 +114,8 @@ class gun():
         else:
             canv.itemconfig(self.id, fill='black')
         canv.coords(self.id, 20, 450,
-                    20 + max(self.f2_power, 20) * math.cos(self.an),
-                    450 + max(self.f2_power, 20) * math.sin(self.an)
+                    x2,
+                    y2
                     )
 
     def power_up(self):
@@ -127,20 +129,47 @@ class gun():
 
 class target():
     def __init__(self):
-        """
-        Инициализация новой цели.
-        """
-
         self.points = 0
         self.live = 1
+        # FIXME: don't work!!! How to call this functions when object is created?
+
+        self.id = canv.create_oval(0, 0, 0, 0)
+        self.rect = canv.create_rectangle(0, 0, 0, 0)
+        self.ug = canv.create_rectangle(0, 0, 0, 0)
+
+        self.id_points = canv.create_text(30, 30, text=self.points, font='28')
+        self.rect_points = canv.create_text(30, 30, text=self.points, font='28')
+        self.ug_points = canv.create_text(30, 30, text=self.points, font='28')
+
+        self.new_target()
+
+    def new_target(self):
+        """ Инициализация новой цели. """
         x = self.x = rnd(600, 780)
         y = self.y = rnd(300, 550)
         r = self.r = rnd(2, 50)
+
+        x1 = self.x = rnd(600, 780)
+        y1 = self.y = rnd(300, 550)
+        r1 = self.r = rnd(2, 50)
+
+        x2 = self.x = rnd(600, 780)
+        y2 = self.y = rnd(300, 550)
+        r2 = self.r = rnd(2, 50)
+
         color = self.color = 'red'
-        self.id = canv.create_oval(0, 0, 0, 0)
-        self.id_points = canv.create_text(30, 30, text=self.points, font='28')
+        color1 = self.color = 'green'
+        color2 = self.color = 'yellow'
+
         canv.coords(self.id, x - r, y - r, x + r, y + r)
+        canv.coords(self.rect, x1 - r1, y1 - r1, x1 + r1, y1 + r1)
+        canv.coords(self.ug, x2 - r2, y2 - r2, x2 + r2, y2 + r2)
+
+
         canv.itemconfig(self.id, fill=color)
+        canv.itemconfig(self.rect, fill=color1)
+        canv.itemconfig(self.ug, fill=color2)
+
 
     def hit(self, points=1):
         """Попадание шарика в цель."""
@@ -148,37 +177,51 @@ class target():
         self.points += points
         canv.itemconfig(self.id_points, text=self.points)
 
+        canv.coords(self.rect, -10, -10, -10, -10)
+        canv.itemconfig(self.rect_points, text=self.points)
+
+        canv.coords(self.ug, -10, -10, -10, -10)
+        canv.itemconfig(self.ug_points, text=self.points)
+
+
+
+
+t1 = target()
+screen1 = canv.create_text(400, 300, text='', font='28')
+g1 = gun()
+bullet = 0
+balls = []
+
 
 def new_game(event=''):
-    global gun, target, screen1, balls, bullet
-
+    global gun, t1, screen1, balls, bullet
+    t1.new_target()
     bullet = 0
     balls = []
-    canv.bind('<Button-1>', gun.fire2_start)
-    canv.bind('<ButtonRelease-1>', gun.fire2_end)
-    canv.bind('<Motion>', gun.targetting)
+    canv.bind('<Button-1>', g1.fire2_start)
+    canv.bind('<ButtonRelease-1>', g1.fire2_end)
+    canv.bind('<Motion>', g1.targetting)
 
-
-    target.live = 1
-    while target.live or balls:
-        for ball in balls:
-            ball.move()
-            if b.hittest(target) and target.live:
-                target.live = 0
-                target.hit()
+    z = 0.03
+    t1.live = 1
+    while t1.live or balls:
+        for b in balls:
+            b.move()
+            if b.hittest(t1) and t1.live:
+                t1.live = 0
+                t1.hit()
                 canv.bind('<Button-1>', '')
                 canv.bind('<ButtonRelease-1>', '')
                 canv.itemconfig(screen1, text='Вы уничтожили цель за ' + str(bullet) + ' выстрелов')
         canv.update()
         time.sleep(0.03)
-        gun.targetting()
-        gun.power_up()
+        g1.targetting()
+        g1.power_up()
     canv.itemconfig(screen1, text='')
     canv.delete(gun)
-    root.after(450, new_game())
+    root.after(750, new_game)
 
 
 new_game()
-
 
 mainloop()
